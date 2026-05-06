a64285b9b0b8f927340f1076c83cfb43f4196eb9
KadinHarris <Kmontrell23@gmail.com>
2026-05-05 19:29:30 -0500

    made the game a game

--- Beautiful_Galaga/game.rkt
+++ Beautiful_Galaga/game.rkt
@@ -2,52 +2,61 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 
-;; Replace element at index i in list lst with value v
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; BASIC UTIL
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define (list-set lst i v)
   (if (= i 0)
       (cons v (cdr lst))
       (cons (car lst) (list-set (cdr lst) (- i 1) v))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CONSTANTS / INITIAL STATE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define WIDTH 400)
 (define HEIGHT 400)
+
 (define PLAYER-SPEED 5)
-(define BLANK-CANVAS (rectangle WIDTH HEIGHT "solid" "white"))
 (define projectile-speed 5)
 (define enemy-speed 2)
+
+(define BLANK-CANVAS (rectangle WIDTH HEIGHT "solid" "white"))
 
 (define projectiles (list))
 (define enemies (list (cons 200 10)))
 
-;                         x           y       lives    speed                           score
-(define state (list (/ WIDTH 2)  (- HEIGHT 10)  3   PLAYER-SPEED  projectiles enemies   0))
-;               idx      0            1         2       3              4        5       6
+(define SPAWN-X (/ WIDTH 2))
+(define SPAWN-Y (- HEIGHT 10))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; ACCeSSORS AND STATERS
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; PLAYER ACCESSORS
-(define (player-x state)
-  (car state))
+;; STATE LAYOUT
+;; idx:     0    1    2      3        4             5         6
+;;        [ x ,  y , lives , speed , projectiles , enemies , score ]
 
-(define (player-y state)
-  (car (cdr state)))
+(define state
+  (list SPAWN-X
+        SPAWN-Y
+        3
+        PLAYER-SPEED
+        projectiles
+        enemies
+        0))
 
-(define (player-lives state)
-  (car (cdr (cdr state))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ACCESSORS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (player-speed state)
-  (car (cdr (cdr (cdr state)))))
+(define (player-x state) (car state))
+(define (player-y state) (car (cdr state)))
+(define (player-lives state) (car (cdr (cdr state))))
+(define (player-speed state) (car (cdr (cdr (cdr state)))))
 
-;(define (extract-player))
-
-
-; PROJECTILE ACCESSORS
 (define (state-projectiles state)
   (car (cdr (cdr (cdr (cdr state))))))
 
 (define (state-projectile state)
-         (car (state-projectiles state)))
+  (car (state-projectiles state)))
 
 (define (state-projectile-x projectile)
   (car projectile))
@@ -55,111 +64,69 @@
 (define (state-projectile-y projectile)
   (car (cdr projectile)))
 
-
-; ENEMY ACCESSORS
 (define (state-enemies state)
   (car (cdr (cdr (cdr (cdr (cdr state)))))))
 
 (define (state-score state)
   (car (cdr (cdr (cdr (cdr (cdr (cdr state))))))))
 
-; PLAYER SETTERS 
-(define (set-player-x s val)
-  (list-set s 0 val))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; SETTERS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (set-player-y s val)
-  (list-set s 1 val))
-
-(define (set-lives s val)
-  (list-set s 2 val))
-
-(define (set-player-speed s val)
-  (list-set s 3 val))
-
-; PROJECTILE SETTERS
-(define (set-projectiles s val)
-  (list-set s 4 val))
-
-; ENEMY SETTERS
-(define (set-enemies s val)
-  (list-set s 5 val))
-
-(define (set-score s val)
-  (list-set s 6 val))
+(define (set-player-x s val) (list-set s 0 val))
+(define (set-player-y s val) (list-set s 1 val))
+(define (set-lives s val) (list-set s 2 val))
+(define (set-player-speed s val) (list-set s 3 val))
+(define (set-projectiles s val) (list-set s 4 val))
+(define (set-enemies s val) (list-set s 5 val))
+(define (set-score s val) (list-set s 6 val))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; CONTROLS
+;; MOVEMENT / CONTROLS
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Takes in the state of the game and moves the player position left by 10
+
 (define (move-left s)
-  (cond [(equal? (player-x s) 0) s] ; return same state
-        [else (set-player-x s (- (player-x s) (player-speed s)))])) ; return state with x - speed
+  (if (equal? (player-x s) 0)
+      s
+      (set-player-x s (- (player-x s) (player-speed s)))))
 
-; Takes in the state of the game and moves the player position right by 10
 (define (move-right s)
-  (cond [(equal? (player-x s) WIDTH) s] ; return same pos
-        [else (set-player-x s (+ (player-x s) (player-speed s)))])) ; return pons with x + speed
+  (if (equal? (player-x s) WIDTH)
+      s
+      (set-player-x s (+ (player-x s) (player-speed s)))))
 
 (define (move-up s)
-  (cond [(equal? (player-y s) 0) s]
-        [else (set-player-y s (- (player-y s) (player-speed s)))]))
+  (if (equal? (player-y s) 0)
+      s
+      (set-player-y s (- (player-y s) (player-speed s)))))
 
 (define (move-down s)
-  (cond [(equal? (player-y s) HEIGHT) s]
-        [else (set-player-y s (+ (player-y s) (player-speed s)))]))
+  (if (equal? (player-y s) HEIGHT)
+      s
+      (set-player-y s (+ (player-y s) (player-speed s)))))
 
-;
-;
-; Places a projectile at the player position
 (define (shoot s)
-  (list-set s 4 (cons (cons (player-x s) (player-y s)) (state-projectiles s)))) ; cons a new position to the projectiles list
+  (list-set s 4
+            (cons (cons (player-x s) (player-y s))
+                  (state-projectiles s))))
 
 (define (spawn-enemy s x y)
-  (list-set s 5 (cons (cons x y) (state-enemies s))))
+  (list-set s 5
+            (cons (cons x y)
+                  (state-enemies s))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; COLLISION / BOUNDS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (on-screen? pos)
-  (cond [(or (> (cdr pos) HEIGHT) (< (cdr pos) 0)) #f]
-        [else #t]))
+  (not (or (> (cdr pos) HEIGHT)
+           (< (cdr pos) 0))))
 
 (define (collides? p e)
   (and (< (abs (- (car p) (car e))) 10)
        (< (abs (- (cdr p) (cdr e))) 5)))
-
-
-; DRAWING 
-; Takes in the current state and an image to draw onto
-(define (draw-player state image)
-  (place-image (circle 5 "solid" "red")
-               (player-x state) (player-y state)
-               image))
-
-; Recursively travels the list of projectiles in state and accumulates an image based on projectile positions
-(define (draw-projectiles projectiles image)
-  (define (helper projectiles acc)
-    (cond [(empty? projectiles) acc]
-          [else (helper (cdr projectiles) (place-image (circle 3 "solid" "green")
-                                                       (car (car projectiles)) (cdr (car projectiles))
-                                                       acc))]))
-  (helper projectiles image))
-
-; Recursively travels the list of enemies in state and accumulates an image based on projectile positions
-(define (draw-enemies enemies image)
-  (define (helper enemies acc)
-    (cond [(empty? enemies) acc]
-          [else (helper (cdr enemies) (place-image (circle 5 "solid" "blue")
-                                                       (car (car enemies)) (cdr (car enemies))
-                                                       acc))]))
-  (helper enemies image))
-
-;
-; ENEMY AND PRJECTILE MOVEMENT
-; moves one projectile's y up by their speed
-(define (move-projectile projectile projectile-speed)
-  (cons (car projectile) (- (cdr projectile) projectile-speed)))
-
-; moves an enemy's y down by their speed
-(define (move-enemy enemy enemy-speed)
-  (cons (car enemy) (+ (cdr enemy) enemy-speed)))
 
 (define (enemy-hit? enemy projectiles)
   (cond [(empty? projectiles) #f]
@@ -173,78 +140,200 @@
   (filter (lambda (p) (not (enemy-hit? p enemies))) projectiles))
 
 (define (player-hit? p-pos enemies)
-   (cond [(empty? enemies) #f]
+  (cond [(empty? enemies) #f]
         [(collides? p-pos (car enemies)) #t]
         [else (player-hit? p-pos (cdr enemies))]))
 
-; updates the positions of the projectiles list and gets rid of any that leave the screen
+(define (remove-player-hit-enemies enemies player-pos)
+  (filter (lambda (e) (not (collides? player-pos e))) enemies))
+
+(define (enemy-reached-bottom? enemies)
+  (ormap (lambda (e) (>= (cdr e) HEIGHT)) enemies))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MOVEMENT UPDATES
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (move-projectile projectile speed)
+  (cons (car projectile)
+        (- (cdr projectile) speed)))
+
+(define (move-enemy enemy speed)
+  (cons (car enemy)
+        (+ (cdr enemy) speed)))
+
 (define (update-proj projs)
-        (filter on-screen? (map (lambda (item) (move-projectile item projectile-speed)) projs)))
+  (filter on-screen?
+          (map (lambda (p)
+                 (move-projectile p projectile-speed))
+               projs)))
 
-; updates the positions of the enemies list and gets rid of any that leave the screen
 (define (update-enemies enemies proj)
-  (filter on-screen? (map (lambda (item) (move-enemy item enemy-speed)) enemies)))
+  (filter on-screen?
+          (map (lambda (e)
+                 (move-enemy e enemy-speed))
+               enemies)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; GAME OVER
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define (game-over? s)
+  (or (<= (player-lives s) 0)
+      (enemy-reached-bottom? (state-enemies s))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; UPDATE LOOP
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Update game state
 (define (update s)
-  (define proj (update-proj (state-projectiles s)))
-  (define e (update-enemies (state-enemies s) proj))
 
-  ; CHECK COLLISIONS
-  (define filtered-enemies (filter-enemies e proj))
-  (define filtered-proj (filter-projectiles proj e))
-  
-  (define new-state
-    (list (player-x s) (player-y s) (player-lives s) (player-speed s) filtered-proj  filtered-enemies (state-score s))
-    )
+  (if (game-over? s)
+      s
+      (let* ([proj (update-proj (state-projectiles s))]
+             [moved-enemies (update-enemies (state-enemies s) proj)]
 
-  (if (empty? filtered-enemies)
-      (spawn-enemy new-state (+ 5 (random (- WIDTH 5))) (random (quotient HEIGHT 3)))
-      new-state)
+             [after-projectile
+              (filter-enemies moved-enemies proj)]
 
-  )
+             [filtered-proj
+              (filter-projectiles proj moved-enemies)]
 
-(provide update)     
+             [projectile-kills
+              (- (length moved-enemies)
+                 (length after-projectile))]
 
-; extracts game datac
-; extracts player data (define p (get-player-data s)
-; extracts/updates projectile data (define proj (update-proj (get-proj s)))
-; extracts enemy data
-;(list (update-game g) p proj (update-enemies e proj))
+             [filtered-enemies
+              (filter on-screen? after-projectile)]
 
+             [new-score
+              (+ (state-score s) projectile-kills)]
 
-; Rednders the current gamestate for current frame
+             [player-pos (cons (player-x s) (player-y s))]
+
+             [hit? (player-hit? player-pos filtered-enemies)]
+
+             [new-lives
+              (if hit?
+                  (max 0 (sub1 (player-lives s)))
+                  (player-lives s))]
+
+             [enemies-after-player
+              (remove-player-hit-enemies filtered-enemies player-pos)]
+
+             [new-x (if hit? SPAWN-X (player-x s))]
+             [new-y (if hit? SPAWN-Y (player-y s))]
+
+             [new-state
+              (list new-x
+                    new-y
+                    new-lives
+                    (player-speed s)
+                    filtered-proj
+                    enemies-after-player
+                    new-score)])
+
+        (if (empty? enemies-after-player)
+            (spawn-enemy new-state
+                         (+ 5 (random (- WIDTH 5)))
+                         (random (quotient HEIGHT 3)))
+            new-state))))
+
+(provide update)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; RENDERING
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (draw-player state image)
+  (place-image (circle 5 "solid" "red")
+               (player-x state)
+               (player-y state)
+               image))
+
+(define (draw-projectiles projectiles image)
+  (define (helper ps acc)
+    (cond [(empty? ps) acc]
+          [else
+           (helper (cdr ps)
+                   (place-image (circle 3 "solid" "green")
+                                (car (car ps))
+                                (cdr (car ps))
+                                acc))]))
+  (helper projectiles image))
+
+(define (draw-enemies enemies image)
+  (define (helper es acc)
+    (cond [(empty? es) acc]
+          [else
+           (helper (cdr es)
+                   (place-image (circle 5 "solid" "blue")
+                                (car (car es))
+                                (cdr (car es))
+                                acc))]))
+  (helper enemies image))
+
 (define (render state)
-  ; access the player position from state, draw it
-  ; access the projectiles list from state, draw them all (recursively?)
-  ; (draw-player (draw-projectiles (draw-enemies background)))
-       (draw-player state (draw-projectiles (state-projectiles state)
-                                             (draw-enemies (state-enemies state) (empty-scene WIDTH HEIGHT)))))
+
+  (define base-scene
+    (draw-player state
+                 (draw-projectiles (state-projectiles state)
+                                   (draw-enemies (state-enemies state)
+                                                 (empty-scene WIDTH HEIGHT)))))
+
+  (if (game-over? state)
+      (place-image
+       (text (string-append "GAME OVER  Score: "
+                            (number->string (state-score state)))
+             30
+             "red")
+       (/ WIDTH 2)
+       (/ HEIGHT 2)
+       (empty-scene WIDTH HEIGHT))
+
+      (place-image
+       (text (string-append "Score: "
+                            (number->string (state-score state)))
+             18
+             "black")
+       60
+       20
+       (place-image
+        (text (string-append "Lives: "
+                             (number->string (player-lives state)))
+              18
+              "black")
+        (- WIDTH 50)
+        20
+        base-scene))))
+
 (provide render)
-  
 
-; CONTROLS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; INPUT
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define (change w a-key)
-  (cond
-    [(key=? a-key "left")  (move-left w)]
-    [(key=? a-key "right") (move-right w)]
-;    [(= (string-length a-key) 1) w] ; order-free checking
-    [(key=? a-key "up")    (move-up w)]
-    [(key=? a-key "down")  (move-down w)]
-    [(key=? a-key " ") (shoot w)]
-    ;[(key=? a-key "s") (spawn-enemy w (random WIDTH) (random (quotient HEIGHT 3)))]
-    [else w]))
+  (if (<= (player-lives w) 0)
+      w
+      (cond
+        [(key=? a-key "left") (move-left w)]
+        [(key=? a-key "right") (move-right w)]
+        ;[(key=? a-key "up") (move-up w)]
+        ;[(key=? a-key "down") (move-down w)]
+        [(key=? a-key " ") (shoot w)]
+        [else w])))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; RUN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; RUN GAME
 (define play
-(big-bang state
-  [on-tick update] ;change enemy data automatically
-  [on-key change] ; change user data based on input
-  [to-draw render]))
+  (big-bang state
+    [on-tick update]
+    [on-key change]
+    [to-draw render]))
+
 (provide play)
 
 (play)
